7278a03d9ad96ef9a906ac3e093d51613652aa35
Walter-Quaglia <82545948+Walter-Quaglia@users.noreply.github.com>
2021-07-05 16:14:55 -0300

    Segundo Modulo Concluido

---
+++ MentoriaDevSTi3/MentoriaDevSTi3/View/Principal.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MentoriaDevSTi3.View.UserControls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MentoriaDevSTi3.View
+{
+    /// <summary>
+    /// Interaction logic for Principal.xaml
+    /// </summary>
+    public partial class Principal : Window
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            InicializarUc(sender);
+        }
+
+        private void InicializarUc(object sender)
+        {
+            if (sender is Button btn)
+            {
+                switch (btn.Name)
+                {
+                    case nameof(BtnProdutos):
+                        Conteudo.Content = new UcProdutos();
+                        break;
+                    case nameof(BtnClientes):
+                        Conteudo.Content = new UcClientes();
+                        break;
+                    case nameof(BtnPedido):
+                        Conteudo.Content = new UcPedido();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
